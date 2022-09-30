6ca5ef81a04c36a411a8f6518a82ddf6f318fdfc
lokinegalur <negalurlohith@gmail.com>
2022-09-30 18:31:55 +0000

    nf

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 filename="/workspace/changed_dirs.txt"
 while read -r line; do
-    dir="$line/"
-    echo "Name read from file - $dir"
+    dir="$line"
+    echo "new pipeline in - $dir"
+    pip3 install -r "$dir/requirements.tx" --user
+    python -m "$dir/code" "$(cat dir/template_args.txt)"
 done < "$filename"
