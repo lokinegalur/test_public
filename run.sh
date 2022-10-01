17eb3c677e614dc004ce94ae1b155137c0d7a717
lokinegalur <negalurlohith@gmail.com>
2022-10-01 16:47:41 +0000

    nf

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ while read -r line; do
     dir="$line"
     echo "new pipeline in - $dir"
     ls $dir
-    pip3 install -r  "$dir/requirements.txt" --user --force-reinstall
-    "$(python $dir/code.py $(cat $dir/template_args.txt|xargs))"
+    pip3 install -r  "$dir/requirements.txt" --user --force-reinstall --no-warn-script-location
+    python $dir/code.py $(cat $dir/template_args.txt|xargs)
 done < "$filename"
