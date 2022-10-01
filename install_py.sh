42f6e596a07b2fdfa3a9f9a30a9f00d836ee381d
lokinegalur <negalurlohith@gmail.com>
2022-10-01 14:40:08 +0000

    nf

--- install_py.sh
+++ install_py.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 echo "Installing python3.8"
+python -V
 apt-get update
-# apt install software-properties-common
-# add-apt-repository ppa:deadsnakes/ppa
-# apt install python3.8
-apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
-cd Python-3.8.0
-./configure --enable-optimizations
-make -j 8
-make altinstall
-echo "python version - $(python3.8 --version)"
+apt install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt install python3.8
+echo "python version - $(python --version)"
 echo "Installing pip3"
 apt-get -y install python3-pip
 echo "pip3 version - $(pip3 --version)"
