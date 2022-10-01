0841d033254ce6c8406495e894926538f60e36cd
lokinegalur <negalurlohith@gmail.com>
2022-10-01 15:01:29 +0000

    nf

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ while read -r line; do
     dir="$line"
     echo "new pipeline in - $dir"
     pip3 install -r  "$dir/requirements.txt" --user --force-reinstall
-    python -m "$dir/code" "$(cat $dir/template_args.txt)"
+    python -m "/workspace/$dir/code" "$(cat $dir/template_args.txt)"
 done < "$filename"
