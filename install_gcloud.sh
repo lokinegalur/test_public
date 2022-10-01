6d6325dc0b407003ba2d30a1687a349ade32e0c6
lokinegalur <negalurlohith@gmail.com>
2022-10-01 17:00:25 +0000

    nf

--- install_gcloud.sh
+++ install_gcloud.sh
@@ -15,4 +15,4 @@
 apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update && sudo apt-get install google-cloud-cli
+apt-get update && apt-get install google-cloud-cli
