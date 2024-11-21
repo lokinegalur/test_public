8bc019c8467a6a170ff0cddbe9a73a645ab6b4c9
lokinegalur <negalurlohith@gmail.com>
2024-11-21 10:08:51 +0000

    test

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import path from "path"
 const app = express()
 
 // DATABASE CONNECTION DETAILS
-const path = path()
+// const path = path()
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 const db_host = process.env.DB_HOST;
 const db_port = process.env.DB_PORT;
