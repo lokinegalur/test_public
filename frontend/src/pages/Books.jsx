4ee592d2417f0bb6662a45a60261644e2633181d
lokinegalur <negalurlohith@gmail.com>
2024-11-21 11:01:35 +0000

    test

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Books = () => {
     return {
-        <div>Books</div>
+        <div>
+        Books
+        </div>
     }
 }
 
--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Update = () => {
     return {
-        <div>Update</div>
+        <div>
+        Update
+        </div>
     }
 }
 
