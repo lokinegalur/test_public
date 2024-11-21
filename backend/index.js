d5da8a1e4dfb422ef3d6c8f93c4cedc2d7e9b167
lokinegalur <negalurlohith@gmail.com>
2024-11-21 09:40:11 +0000

    test

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,22 @@ app.get("/books",(req,res)=>{
     })
 })
 
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+
 app.listen(8800, ()=>{
     console.log("Connected to backend :)")
 })
