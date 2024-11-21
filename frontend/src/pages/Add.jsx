195206639f23906d2d3ba1fa747d2b07fde18a93
lokinegalur <negalurlohith@gmail.com>
2024-11-21 11:00:30 +0000

    test

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Add = () => {
-    return {
-        <div>Add</div>
-    }
+    return (
+        <div>
+        Add
+        </div>
+    )
 }
 
 export default Add
