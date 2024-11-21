a9097608f285774fcfd861edf1f95713dfcfafbb
lokinegalur <negalurlohith@gmail.com>
2024-11-21 10:58:19 +0000

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-
+import Add from "./pages/Add";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
 function App() {
   return (
     <div className="App">
