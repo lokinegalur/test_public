59ccf8022e3aae9ae5b572525449e2108e6ede07
lokinegalur <negalurlohith@gmail.com>
2024-11-21 10:55:51 +0000

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      Hello
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Books/>}/>
+        <Route path="/add" element={<Add/>}/>
+        <Route path="/update" element={<Update/>}/>
+      <Routes>
+    </BrowserRouter>
     </div>
   );
 }
