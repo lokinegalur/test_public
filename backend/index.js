89fd9d4664d176f2769e37931e5ef18f08f65da7
lokinegalur <negalurlohith@gmail.com>
2024-11-21 09:10:10 +0000

    backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mysql from "mysql"
+const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "lohith",
+    password: "XXXXXX",
+    database: "test"
+})
+
+app.get("/",(req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/books",(req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend :)")
+})
